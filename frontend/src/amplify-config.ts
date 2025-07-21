86aaff6c01b7242a9dd92c80a4b25f29391c8303
Hiro Sato <hiro_sato@me.com>
2025-07-21 23:12:31 +0900

    added cognito login

---
+++ frontend/src/amplify-config.ts
@@ -0,0 +1,9 @@
+const amplifyConfig = {
+    aws_project_region: "us-west-2",
+    aws_cognito_region: "us-west-2",
+    aws_user_pools_id: "us-west-2_xxxxxxxxxx",
+    aws_user_pools_web_client_id: "xxxxxxxxxxxxxxxxxxxxxxx",
+    federationTarget: "COGNITO_USER_POOLS",
+  };
+  
+  export default amplifyConfig;
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,15 +1,25 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
-import Header from '../components/Header'
+import amplifyConfig from '@/amplify-config';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import Header from '../components/Header';
+
+Amplify.configure({ ...amplifyConfig });
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Header />
+      <Authenticator>
+        <Authenticator.Provider>
+          <Header />
 
-      <Outlet />
-      <TanStackRouterDevtools />
+          <Outlet />
+          <TanStackRouterDevtools />
+        </Authenticator.Provider>
+      </Authenticator>
     </>
   ),
 })
